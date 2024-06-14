5bf69247f9ffb8af51ff7a2a57434e75099f4d77
cuongnvtse160875@gmail.com <cuongnvtse160875@gmail.com>
2024-06-14 15:10:52 +0700

    323

--- src/controllers/sui/sui.service.ts
+++ src/controllers/sui/sui.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common"
+import { BadRequestException, Injectable } from "@nestjs/common"
 import {
     GetTokenByteCodeRequestBody,
     GetTokenByteCodeResponseData,
@@ -9,7 +9,7 @@ import { readFileSync, writeFileSync } from "fs"
 
 @Injectable()
 export class SuiService {
-    constructor(private readonly shellService: ShellService) {}
+    constructor(private readonly shellService: ShellService) { }
 
     async getTokenByteCode({
         decimals,
@@ -22,35 +22,39 @@ export class SuiService {
         const tokenModulePath = join(process.cwd(), "sui-modules", "token")
         const sourcePath = join(tokenModulePath, "sources", "token.move")
         const source = readFileSync(sourcePath, "utf-8")
-        let sourceCopy = source.slice()
 
-        const replace = (searchValue: string, replaceValue: string) => {
-            console.log(sourceCopy.indexOf(searchValue))
-            sourceCopy = sourceCopy.replace(searchValue, replaceValue)
+        try {
+            let sourceCopy = source.slice()
+
+            const replace = (searchValue: string, replaceValue: string) => {
+                console.log(sourceCopy.indexOf(searchValue))
+                sourceCopy = sourceCopy.replace(searchValue, replaceValue)
+            }
+            console.log(sourceCopy)
+            replace("let template_decimal: u8 = 0;", `let template_decimal: u8 = ${decimals};`)
+            replace("let template_name: vector<u8> = b\"USD Tether\";", `let template_name: vector<u8> = b"${name}";`)
+            replace("let template_symbol: vector<u8> = b\"USDT\";", `let template_symbol: vector<u8> = b"${symbol}";`)
+            replace("let template_description: vector<u8> = vector::empty<u8>();", `let template_description: vector<u8> = b"${description}";`)
+            replace("let template_icon_url: vector<u8> = vector::empty<u8>();", `let template_icon_url: vector<u8> = b"${iconUrl}";`)
+            replace("let template_total_supply: u64 = 10000000000;", `let template_total_supply: u64 = ${totalSupply};`)
+
+            writeFileSync(sourcePath, sourceCopy)
+
+            const { modules, dependencies, digest } = JSON.parse(
+                this.shellService.execSync(
+                    `sui move build --path ${sourcePath} --dump-bytecode-as-base64`,
+                ),
+            )
+            return {
+                modules,
+                dependencies,
+                digest,
+            }
+        } catch (ex) {
+            throw new BadRequestException("Server error")
+        } finally {
+            writeFileSync(sourcePath, source)
         }
-        console.log(sourceCopy)
-        replace("let template_decimal: u8 = 0;", `let template_decimal: u8 = ${decimals};`)
-        replace("let template_name: vector<u8> = b\"USD Tether\";", `let template_name: vector<u8> = b"${name}";`)
-        replace("let template_symbol: vector<u8> = b\"USDT\";", `let template_symbol: vector<u8> = b"${symbol}";`)
-        replace("let template_description: vector<u8> = vector::empty<u8>();", `let template_description: vector<u8> = b"${description}";`)
-        replace("let template_icon_url: vector<u8> = vector::empty<u8>();", `let template_icon_url: vector<u8> = b"${iconUrl}";`)
-        replace("let template_total_supply: u64 = 10000000000;", `let template_total_supply: u64 = ${totalSupply};`)
 
-        console.log(sourceCopy)
-        writeFileSync(sourcePath, sourceCopy)
-
-        const { modules, dependencies, digest } = JSON.parse(
-            this.shellService.execSync(
-                `sui move build --path ${sourcePath} --dump-bytecode-as-base64`,
-            ),
-        )
-
-        writeFileSync(sourcePath, source)
-
-        return {    
-            modules,
-            dependencies,
-            digest,
-        }
     }
 }
